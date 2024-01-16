8d0584c8dce22ac7c4d1792a6c474e8891bdb17b
azizollah <azizollahpayandeh1@gmail.com>
2024-01-16 23:09:20 +0330

    i made menu

---
+++ app/Components/Modules/Menu/MenuNav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <>
+    <div className='relative'>
+      <div className='w-[150px] h-auto bg-gradient-to-r blur-[0.2px] rounded-md bg-opacity-20 from-[#71717c]  to-[#4c3483]  absolute '>
+        <ul className='flex flex-col gap-5 m-3'>
+          <li className='hover:text-[19px] text-[18px] transition-all duration-200 cursor-pointer'>Home</li>
+          <li className='hover:text-[19px] text-[18px] transition-all duration-200 cursor-pointer'>Contact</li>
+          <li className='hover:text-[19px] text-[18px] transition-all duration-200 cursor-pointer'>About</li>
+          <li className='hover:text-[19px] text-[18px] transition-all duration-200 cursor-pointer'>Sign Up</li>
+        </ul>
+      </div>
+    </div>
+    </>
+  )
+}
--- app/Components/Template/Navbar/Navbar.tsx
+++ app/Components/Template/Navbar/Navbar.tsx
@@ -4,37 +4,61 @@ import { IoSearch } from "react-icons/io5";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { HiOutlineShoppingCart } from "react-icons/hi2";
 import { IoMdMenu } from "react-icons/io";
+import { useEffect, useRef, useState } from "react";
+import Menu from "../../Modules/Menu/MenuNav";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-const handelclick = () => {
-  console.log('hello');
-  
-}
+  const path = usePathname()
+
+const [clickHandlerMenu, setClickHandlerMenu] = useState(false);
+const toggleMenu = () => {
+  setClickHandlerMenu(!clickHandlerMenu);
+};
+const handleDocumentClick = (event: MouseEvent) => {
+  if (clickHandlerMenu && menuRef.current && !menuRef.current.contains(event.target as Node)) {
+    setClickHandlerMenu(false);
+  }
+};
+
+useEffect(() => {
+  document.addEventListener("click", handleDocumentClick)
+
+  return () => {
+    document.removeEventListener("click", handleDocumentClick);
+  };
+}, [clickHandlerMenu])
+
+const menuRef = useRef<HTMLDivElement | null>(null);
+
 
   return (
     <>
       <header className="bg-white py-4">
         <div className=" mx-auto">
           <div className="flex justify-between items-center w-full border-b-2 border-gray-100 py-3">
+            <div>
+            <Image alt="logo" width={170} height={50} src="/assets/azizkala-logo-black.png"/>
+            </div>
             <div className="flex items-center lg:space-x-20 space-x-5">
-              <h1 className="text-3xl font-bold">Exclusive</h1>
-              <nav className="hidden md:flex space-x-6">
-                <Link className="text-gray-500 font-[500] hover:text-gray-700" href="#">
+              <nav className="hidden lg:flex space-x-6">
+                <Link className={`text-gray-500 font-[500] hover:text-gray-700 ${path == "/" ? "border-b-2" : ""} `} href="/">
                   Home
                 </Link>
-                <Link className="text-gray-500 font-[500] hover:text-gray-700" href="#">
+                <Link className={`text-gray-500 font-[500] hover:text-gray-700 ${path == "/concat" ? "border-b-2" : ""} `} href="/concat">
                   Contact
                 </Link>
-                <Link className="text-gray-500 font-[500] hover:text-gray-700" href="#">
+                <Link className={`text-gray-500 font-[500] hover:text-gray-700 ${path == "/about" ? "border-b-2" : ""} `} href="/about">
                   About
                 </Link>
-                <Link className="text-gray-500 font-[500] hover:text-gray-700" href="#">
+                <Link className={`text-gray-500 font-[500] hover:text-gray-700 ${path == "/sign-up" ? "border-b-2" : ""} `} href="/sign-up">
                   Sign Up
                 </Link>
               </nav>
             </div>
             <div className="flex items-center space-x-4">
-              <div className="hidden md:flex items-center bg-gray-100 rounded-md overflow-hidden px-10 py-2">
+              <div className="hidden lg:flex items-center bg-gray-100 rounded-md overflow-hidden px-10 py-2">
                 <IoSearch className="text-gray-500 w-[18px]" />
                 <input
                   className="bg-transparent border-none outline-none placeholder-gray-500 text-sm"
@@ -44,11 +68,12 @@ const handelclick = () => {
               </div>
               <IoMdHeartEmpty className="text-gray-500 cursor-pointer" fontSize={25}  />
               <HiOutlineShoppingCart className="text-gray-500 cursor-pointer" fontSize={25}/>
-              <IoMdMenu className="text-gray-500 md:hidden cursor-pointer"  fontSize={25}/>
+              <IoMdMenu className="text-gray-500 lg:hidden cursor-pointer"  onClick={toggleMenu}  fontSize={25}/>
             </div>
           </div>
-          <button onClick={handelclick}>dsfdfkd</button>
-          {/* mobile nav design here*/}
+          <div className={`menu flex justify-end lg:hidden pr-[150px] ${clickHandlerMenu ? "block" : "hidden"}`} ref={menuRef}>
+          <Menu/>
+        </div>
         </div>
       </header>
     </>
